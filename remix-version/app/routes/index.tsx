4c5c16d2792dcbb18012717c7630d8e4a68ae59a
Bond <shrekuu@gmail.com>
2023-03-05 09:12:27 +0800

    fix useLoaderData import error

--- remix-version/app/routes/index.tsx
+++ remix-version/app/routes/index.tsx
@@ -1,37 +1,21 @@
-import { useState, Suspense, use } from "react";
+import { Suspense, useState, use } from "react";
 import { useLoaderData } from "@remix-run/react";
 import { defer } from "@remix-run/node";
-
 import styles from "../../styles/globals.css";
 
 const fetchDescription = () =>
-  new Promise((resolve) =>
+  new Promise<string>((resolve) =>
     setTimeout(() => resolve("Product information ready for SEO"), 100)
   );
 
-const commentsFetch = () =>
-  new Promise((resolve) =>
+const fetchComments = () =>
+  new Promise<string[]>((resolve) =>
     setTimeout(() => resolve(["Comment 1", "Comment 2", "Comment 3"]), 2000)
   );
 
-export function links() {
-  return [{ rel: "stylesheet", href: styles }];
-}
-
-export async function loader() {
-  const description = await fetchDescription();
-  const comments = commentsFetch() as Promise<string[]>;
-
-  return defer({
-    description,
-    comments,
-  });
-}
-
-function Comments({ comments: commentsPromise }: { comments: string[] }) {
+function Comments({ comments: commentPromise }: { comments: string[] }) {
   const [newComment, setNewComment] = useState("");
-  const [comments, setComments] = useState<string[]>(use(commentsPromise));
-
+  const [comments, setComments] = useState<string[]>(use(commentPromise));
   return (
     <div>
       <ul>
@@ -39,6 +23,7 @@ function Comments({ comments: commentsPromise }: { comments: string[] }) {
           <li key={comment}>{comment}</li>
         ))}
       </ul>
+
       <input
         type="text"
         value={newComment}
@@ -56,22 +41,34 @@ function Comments({ comments: commentsPromise }: { comments: string[] }) {
   );
 }
 
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+export async function loader() {
+  const description = await fetchDescription();
+  const comments = fetchComments() as Promise<string[]>;
+
+  return defer({
+    description,
+    comments,
+  });
+}
+
 export default function Index() {
   const { description, comments } = useLoaderData();
 
   return (
-    <div>
+    <>
       <header>Header</header>
 
       <h2>Product Description</h2>
       <p>{description}</p>
-
-      <h2>Comments</h2>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>loading...</div>}>
         <Comments comments={comments} />
       </Suspense>
 
       <footer>Footer</footer>
-    </div>
+    </>
   );
 }
